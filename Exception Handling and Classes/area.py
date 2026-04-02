159c197508bd932e5986dc90f9e8e934ac08eacd
Devendra Choudhary <devendra20051010@gmail.com>
2026-04-02 18:08:07 +0530

    Deleted Useless files

--- Exception Handling and Classes/area.py
+++
@@ -1,8 +0,0 @@
-def calculate_area(base,height):
-    print("__name__: ", __name__)
-    return base * height / 2
-
-if __name__ == "__main__":
-    print("I am in area.py")
-    area = calculate_area(10,20)
-    print("Area:", area)
